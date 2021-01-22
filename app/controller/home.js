dd2a7e8e25a7ac6b2f11a6770e500f3fbf63caf0
delaprada <924265424zzr@sina.com>
2021-01-22 22:52:27 +0800

    feat: 添加生成token及token解密功能

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,9 +1,12 @@
 const Controller = require('egg').Controller;
 const crypto = require('crypto');
+const jwt = require('jwt-simple');
 
-function encryptSha1(data) {
-  return crypto.createHash('sha1').update(data, 'utf8').digest('hex');
-}
+const SECRET = 'zhuoran';
+
+// function encryptSha1(data) {
+//   return crypto.createHash('sha1').update(data, 'utf8').digest('hex');
+// }
 
 class HomeController extends Controller {
   async index() {
@@ -15,16 +18,32 @@ class HomeController extends Controller {
     const { ctx } = this;
     console.log(ctx.request.body);
     const data = ctx.request.body;
+    const userInfo = data.userInfo;
+    console.log(userInfo);
     const res = await ctx.curl(
       `https://api.weixin.qq.com/sns/jscode2session?appid=wx6936c18b38186cf3&secret=d11f77fb7d5a959b6ba46c30dbd4da95&js_code=${data.code}&grant_type=authorization_code`,
       {
         dataType: 'json',
       }
     );
-    const { session_key } = res.data;
-    const skey = encryptSha1(session_key);
-    console.log(skey);
-    ctx.body = skey;
+    // const { session_key } = res.data;
+    // const skey = encryptSha1(session_key);
+    // console.log(skey);
+
+    const { openid } = res.data;
+    console.log('openid为');
+    console.log(openid);
+    const token = jwt.encode(openid, SECRET);
+    ctx.body = token;
+  }
+
+  async request() {
+    const { ctx } = this;
+    console.log(ctx.get('authorization'));
+    const token = ctx.get('authorization');
+    const openid = jwt.decode(token, SECRET);
+    console.log(openid);
+    ctx.body = '请求成功';
   }
 }
 
--- app/router.js
+++ app/router.js
@@ -2,4 +2,5 @@ module.exports = (app) => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
   router.post('/login', controller.home.login);
+  router.get('/request', controller.home.request);
 };
